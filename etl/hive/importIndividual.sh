8504aa799cd88e47e015cd7829ed84f52c33a5ba
Charity A Hilton <charity.a.hilton@gmail.com>
2016-02-02 10:37:39 -0500

    Update importIndividual.sh

--- etl/hive/importIndividual.sh
+++ etl/hive/importIndividual.sh
@@ -1,3 +1,3 @@
-
-sqoop import -m 1 --connect jdbc:postgresql://laertes.ohdsi.org:5432/vocabularyv5 --username=username -P --table person --hive-import     --hive-table cdmv5.person
-
+sqoop import -m 1 --connect jdbc:postgresql://laertes.ohdsi.org:5432/vocabularyv5 --username=username -P --table attribute_definition --hive-import
+sqoop import -m 4 --connect jdbc:postgresql://laertes.ohdsi.org:5432/vocabularyv5 --username=username -P --table care_site --split-by care_site_id --hive-import
+sqoop import -m 4 --connect jdbc:postgresql://laertes.ohdsi.org:5432/vocabularyv5 --username=username -P --table cohort --split-by cohort_definition_id --hive-import
